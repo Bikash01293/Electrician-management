f2cfccd607eba3f548c5a344d90851a235d92aa4
Bikash01293 <adhikarbikash821@gmail.com>
2023-12-09 23:28:03 +0530

    logically handled the electrican allotment

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import electricianData from './data/electricianData.json';
+import rawSiteData from './data/rawSiteData.json';
+
 import './App.css';
 
 function App() {
+  const [electricians, setElectricians] = useState(() =>
+    electricianData.map(electrician => ({
+      ...electrician,
+      sitesAlloted: [],
+    }))
+  );
+
+  const [sites, setSites] = useState(rawSiteData);
+
+  const assignElectricians = () => {
+
+    sites.forEach(site => {
+
+      if (!site.AssignedElectritian.length) {
+        const eligibleElectricians = electricians.filter(
+          e => (!site.grievance && !e.grievanceElectrician) || (site.grievance && e.grievanceElectrician)
+        );
+
+        if (eligibleElectricians.length > 0) {
+
+          const assignedElectrician = eligibleElectricians.shift();
+
+          site.AssignedElectritian.push({
+            electricianName: assignedElectrician.name,
+            electricianAssignDate: new Date().toISOString(),
+          });
+
+          assignedElectrician.sitesAlloted.push(site);
+        }
+      }
+    });
+
+    setSites([...sites]);
+
+    console.log('Electricians allotted:', electricians);
+    console.log('Sites after allotment:', sites);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={assignElectricians}>
+        Allot Electrician Automatically
+      </button>
     </div>
   );
 }
